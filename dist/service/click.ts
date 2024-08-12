b9313e06811b14ed24c8a119acba0769f58f86ad
lujiashuai.777 <lujiashuai.777@bytedance.com>
2024-08-13 00:49:01 +0800

    xx

--- dist/service/click.ts
+++
@@ -1,8 +0,0 @@
-import {insertClickRecord} from "../database/models/article_click";
-
-
-async function insertClick(article_id: number, user_id: string) {
-    return await insertClickRecord(article_id, user_id);
-}
-
-export { insertClick }
